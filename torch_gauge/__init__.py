1136af05e2d6af1755251541043f5d4ac7c352c0
minhtoo <linmin.htoo@gmail.com>
2023-02-28 16:31:41 +0800

    fix: do not write to src dir but write to /tmp/ instead

--- torch_gauge/__init__.py
+++ torch_gauge/__init__.py
@@ -1,8 +1,11 @@
 import os
+import tempfile
 from torch_gauge.verlet_list import VerletList
 from torch_gauge._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = tempfile.TemporaryDirectory()
+print(f"torch gauge: o3_cache at: {ROOT_DIR}")
